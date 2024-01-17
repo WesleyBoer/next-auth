4b9ba5e7bd86af3b996009d59c8f58280a5dffbf
Wesley <wesleysboer@gmail.com>
2024-01-17 16:22:52 -0300

    feat: settings page

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,4 +1,46 @@
+import { newPassword } from "@/actions/new-password";
+import { UserRole } from "@prisma/client";
 import * as z from "zod";
+
+export const SettingsSchema = z
+  .object({
+    name: z.optional(z.string()),
+    isTwoFactorEnabled: z.optional(z.boolean()),
+    role: z.enum([UserRole.ADMIN, UserRole.USER]),
+    email: z.optional(z.string().email()),
+    password: z.optional(z.string().min(6)),
+    newPassword: z.optional(
+      z.string().min(6, {
+        message: "Senha deve ter no mínimo 6 caracteres",
+      })
+    ),
+  })
+  .refine(
+    (data) => {
+      if (data.newPassword && !data.password) {
+        return false;
+      }
+
+      return true;
+    },
+    {
+      message: "Senha é obrigatória!",
+      path: ["password"],
+    }
+  )
+  .refine(
+    (data) => {
+      if (data.password && !data.newPassword) {
+        return false;
+      }
+
+      return true;
+    },
+    {
+      message: "Nova senha é obrigatória!",
+      path: ["newPassword"],
+    }
+  );
 
 export const NewPasswordSchema = z.object({
   password: z.string().min(6, {
