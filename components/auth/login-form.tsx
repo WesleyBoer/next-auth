0550d77472fdeee1758cda559893408e90f75d53
Wesley <wesleysboer@gmail.com>
2024-01-17 19:22:45 -0300

    feat: login modal e callbackUrl após login

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -26,6 +26,7 @@ import Link from "next/link";
 
 export const LoginForm = () => {
   const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
   const urlError =
     searchParams.get("error") === "OAuthAccountNotLinked"
       ? "E-mail já cadastrado em outra conta"
@@ -48,7 +49,7 @@ export const LoginForm = () => {
     setSuccess("");
 
     startTransition(() => {
-      login(values)
+      login(values, callbackUrl)
         .then((data) => {
           if (data?.error) {
             form.reset();
